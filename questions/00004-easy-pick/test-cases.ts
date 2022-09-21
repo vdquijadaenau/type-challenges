e7ff2f2dc6e67b38ad6bdd99362dd4e520c83e46
vdquijadaenau <vdquijadaenau@gmail.com>
2022-09-21 17:28:47 +1000

    question 4

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,10 +1,23 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type MyPick<TObj, TKey extends keyof TObj> = {
+  [Key in TKey]: TObj[Key]
+}
+
+/*
+ * 1. Ensure TKey is restricted to the keys of TObj
+ * 2. Create a mapping {}
+ * 3 We're goign to iterate over the keys of Tobj and pick the specific Keys from Tkey
+ * 4. grab the value for the key
+ */
+
+type ta = MyPick<{ a: number; b: number }, "a">
 
 type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
   // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+  MyPick<Todo, "title" | "completed" | "invalid">
 ]
 
 interface Todo {
